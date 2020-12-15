dd323fc05b9d082fedb484e42ff2e9060cbd6c75
karkir0003 <karkir0003@gmail.com>
2020-12-14 19:10:27 -0500

    updated index.html button'

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/picalc')
+def picalc():
+    return render_template('PiCalc.html')
 
 if __name__ == '__main__':
    app.run()
